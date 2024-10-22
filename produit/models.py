24ab70841687ad9015bfa5cdbb5b7763c4503e59
M Hassan BIKIENGA <m9bikienga@gmail.com>
2024-10-22 20:45:41 +0000

    creation model produi

--- produit/models.py
+++ produit/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Produit(models.Model) :
+    nom = models.CharField(max_length=255)
+    description = models.TextField(max_length=255, null=True , blank=True)
+    prix = models.DecimalField(max_digits=10, decimal_places=2)
+    quantite = models.PositiveIntegerField()
+    date_ajout = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.nom
